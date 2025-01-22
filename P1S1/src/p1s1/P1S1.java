35afdf06062014087a05ef74b5846ec159868508
Dmurillo88 <danielmuri815@gmail.com>
2025-01-22 17:51:47 -0600

    Archivos de Semana 1

---
+++ P1S1/src/p1s1/P1S1.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package p1s1;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Daniel
+ */
+public class P1S1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int miPrimeraVariable;
+        String miSegundaVariable;
+        
+        miPrimeraVariable=99;
+        miSegundaVariable="Clase 1 GRUPO";
+        
+        String variable="HaHaHaHa";
+        String Variable="HaHaHaHA";
+        String VARIABLE="HaHaHaHa";
+        //Esto es un comentario
+        
+        String nombre;
+        int edad;
+        float salario;
+        double temperatura;
+        char inicial;
+        
+        nombre=JOptionPane.showInputDialog("Digite su nombre");
+    }
+    
+}
+
