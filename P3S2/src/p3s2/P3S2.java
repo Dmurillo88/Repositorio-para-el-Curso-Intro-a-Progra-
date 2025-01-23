ea9500e527399c11f53054e2083b964773f99aa9
Dmurillo88 <danielmuri815@gmail.com>
2025-01-22 20:36:24 -0600

    Practica 3 Semana 2

---
+++ P3S2/src/p3s2/P3S2.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package p3s2;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Daniel
+ */
+public class P3S2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int edad =0;
+        edad= Integer.parseInt(JOptionPane.showInputDialog("Dime tu edad"));
+        if (edad >=18){
+          JOptionPane.showMessageDialog(null,"Tomar y Votar");
+        }  
+        else
+         JOptionPane.showMessageDialog(null,"Muy joven!");
+        }
+        
+        
+    
+ }
