5e5ed9eca0301c8d11b9bedfefd1b4d765099209
Dmurillo88 <danielmuri815@gmail.com>
2025-01-29 19:57:08 -0600

    Practica 1 Semana 3

---
+++ P1S3/src/p1s3/P1S3.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package p1s3;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Daniel
+ */
+public class P1S3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String resultado="";
+        for (int i = 0; i < 5; i++) {
+            resultado+= "Valor i: "+ i+"\n";
+                    
+            
+        }
+        
+        JOptionPane.showMessageDialog(null, resultado);
+                
+    }
+    
+    
+    
+ 
+}
