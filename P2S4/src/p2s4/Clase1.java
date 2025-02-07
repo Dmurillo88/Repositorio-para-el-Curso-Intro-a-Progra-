0c8d95ab5226e8be5ea6cb1dc86ecdc322636d03
Dmurillo88 <danielmuri815@gmail.com>
2025-02-07 16:28:59 -0600

    Practica 2 Semana 4

---
+++ P2S4/src/p2s4/Clase1.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package p2s4;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Daniel
+ */
+public class Clase1 {
+    public String color;
+    public String marca;
+    public int anio;
+    public String modelo;
+    private String VIN;
+    
+    //Metodos o acciones
+    public void rodar(){
+        JOptionPane.showMessageDialog(null,"Voy a MIL KM POR HORA!!");
+    }
+    
+    public String tocarelpito(){
+        String resultado="";
+        resultado="PIIIIIIIIII";
+        return resultado;
+        
+    }
+}
